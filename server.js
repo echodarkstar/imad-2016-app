b159b9489b4816610ca76b4c892a9059c24643be
Nishant Shankar <nishantshankar226@gmail.com>
2016-10-30 12:42:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/fog.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'fog.jpg'));
 });
 
 app.get('/page1.html', function(req,res) {
