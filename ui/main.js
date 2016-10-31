fc9a794e9a6cb61de3ae18411e5f2e6fb850db8c
Nishant Shankar <nishantshankar226@gmail.com>
2016-10-31 22:38:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,28 @@
 $(function(){
 	var a=0;
+
+	$('.name').one('webkitAnimationEnd oAnimationEnd msAnimationEnd Animationend animationend',   
+	  function() {
+	    
+	    $('.hexagon').toggleClass('activate');
+
+	});
 	$('.hex1').click(function(){
 		$(this).toggleClass('hex1ani');
 		$(this).siblings().toggleClass('hidden');
 		$('.about-photo').toggleClass('aboutani');
 		$('.about-photo').siblings().toggleClass('abani');
-	})
+	});
 
 	$('.hex2').click(function(){
 		$(this).toggleClass('hex2ani');
 		$(this).siblings().toggleClass('hidden');
 		$('.work').toggleClass('workani');
-	})
+	});
 
 	$('.hex3').click(function(){
 		$(this).toggleClass('hex3ani');
 		$(this).siblings().toggleClass('hidden');
 		$('.contact').toggleClass('conani');
-	})
-})
+	});
+});
