9e84279453a6ed6564fa7588da103adc0a9d7088
echodarkstar <nishantshankar226@gmail.com>
2016-11-17 21:16:25 +0530

    Adding components

---
+++ server.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+var lorem= `
+     <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nisi ultricies, sodales leo eu, commodo libero. Praesent blandit est dui, quis venenatis magna placerat at. Etiam ac imperdiet lorem, consectetur ullamcorper metus. Donec id libero sit amet nulla tempus aliquam a quis mauris. Nulla facilisis ligula ut commodo faucibus. Curabitur iaculis interdum sem. In ac malesuada elit. Vivamus eu elit quis metus cursus molestie.
+    </p>
+    <hr/>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nisi ultricies, sodales leo eu, commodo libero. Praesent blandit est dui, quis venenatis magna placerat at. Etiam ac imperdiet lorem, consectetur ullamcorper metus. Donec id libero sit amet nulla tempus aliquam a quis mauris. Nulla facilisis ligula ut commodo faucibus. Curabitur iaculis interdum sem. In ac malesuada elit. Vivamus eu elit quis metus cursus molestie.
+    </p>
+    `;
+var pageone = {
+    title: 'Page 1 | Nishant\'s webapp',
+    heading: 'Page 1',
+    content: lorem 
+};
+var pagetwo = {
+    title: 'Page 2 | Nishant\'s webapp',
+    heading: 'Page 2',
+    content: lorem 
+};
+var pagethree = {
+    title: 'Page 3 | Nishant\'s webapp',
+    heading: 'Page 3',
+    content: lorem 
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemp = `
+    <html>
+        <head>
+    	    <title>
+    	        ${title} 
+    	    </title>
+    	    <meta name="viewport" content="width=device-width, initial-scale=1">
+    	    <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+            	<div>
+            	    <a href="/">Home</a>
+            	</div>
+            	<hr/>
+            	<h3>
+            	    ${heading}
+            	</h3>
+            	<div>
+            	   ${content}
+            	</div>
+            </div>
+        </body>
+    </html>    
+    `;
+    
+    return htmlTemp;
+}
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/page1.html', function(req,res) {
+    res.send(createTemplate(pageone));
+});
+
+app.get('/page2.html', function(req,res) {
+    res.send(createTemplate(pagetwo));
+});
+
+app.get('/page3.html', function(req,res) {
+    res.send(createTemplate(pagethree));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
---
+++ ui/main.js
@@ -0,0 +1,34 @@
+$(function(){
+	$('.work').find('a').bind('click',false);
+	$('.work').find('a').addClass('disable');
+	$('p').toggleClass('.noselect');
+	$('.hex1').click(function(){
+		$(this).toggleClass('hex1ani');
+		$(this).siblings().toggleClass('hidden');
+		$('.about-photo').toggleClass('aboutani');
+		$('.about-photo').siblings().toggleClass('abani');
+		$(this).find('p').toggleClass('hide-text');
+		$(this).find('span').toggleClass('glyphicon').toggleClass('glyphicon-arrow-down');
+		$('p').toggleClass('.noselect');
+	});
+
+	$('.hex2').click(function(){
+		$(this).toggleClass('hex2ani');
+		$(this).siblings().toggleClass('hidden');
+		$('.work').toggleClass('workani');
+		$(this).find('p').toggleClass('hide-text');
+		$(this).find('span').toggleClass('glyphicon').toggleClass('glyphicon-arrow-right');
+		$('.work').find('a').unbind('click',false);
+		$('.work').find('a').toggleClass('disable');
+		
+	});
+
+	$("#login").click(function(){
+	        $("#myModal").modal();
+	});
+
+	$('[data-toggle="popover"]').popover();
+
+	
+
+});
